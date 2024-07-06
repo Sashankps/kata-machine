60389b44fe7eec1b1bad86fb2a22124ed4d5aded
Sashankps <Sashank.pilli@gmail.com>
2024-07-06 21:54:39 +0530

    refactor: Implement binary search algorithm in BinarySearchList.ts

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let left: number = 0;
+    let right: number = haystack.length - 1;
+    while (left <= right) {
+        let middle: number = left + Math.floor((right - left) / 2);
+        if (haystack[middle] === needle) return true;
+        if (haystack[middle] < needle) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return false;
 }
