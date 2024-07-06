7d661564b26ffb9025d218b2ee977bd43c7fea80
Sashankps <Sashank.pilli@gmail.com>
2024-07-06 17:08:28 +0530

    refactor: Optimize linear search algorithm implementation

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -2,5 +2,13 @@ export default function linear_search(
     haystack: number[],
     needle: number,
 ): boolean {
-    return true;
+    // haystack.forEach( (element : number) => {
+    //     if(element === needle) return true;
+    // })
+    // return false;
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) return true;
+    }
+    return false;
 }
+``;
